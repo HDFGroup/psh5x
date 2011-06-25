290918ae19f097a3be9c2640bfdbcf364963ea37
gheber <gheber@bc1736a7-42db-4d61-a5e1-31a1cdc50de3>
2011-06-25 13:04:37 +0000

    Read compound types with atomic members. Reflection does the trick for the moment. Can we do better?
    git-svn-id: https://svn.hdfgroup.org/PSH5X/trunk@14 bc1736a7-42db-4d61-a5e1-31a1cdc50de3

--- src/DatasetReader.cpp
+++ src/DatasetReader.cpp
@@ -8,10 +8,14 @@ extern "C" {
 #include "H5Tpublic.h"
 }
 
+#include <cstdlib>
+#include <cstring>
+
 using namespace System;
 using namespace System::Collections;
 using namespace System::Management::Automation;
 using namespace System::Management::Automation::Provider;
+using namespace System::Reflection;
 using namespace System::Runtime::InteropServices;
 
 namespace PSH5X
@@ -56,33 +60,113 @@ namespace PSH5X
                 dims[0] = static_cast<hsize_t>(npoints);
                 hid_t mem_space = H5Screate_simple(1, dims, NULL);
 
-                char* buf = new char [npoints*size];
+                array<unsigned char>^ mbuf = gcnew array<unsigned char>(npoints*size);
+
+                pin_ptr<unsigned char> p = &mbuf[0];
+                unsigned char* buf = p;
 
                 if (H5Dread(dset, mem_type, mem_space, H5S_ALL, H5P_DEFAULT, buf) < 0)
                 {
-                    delete [] buf;
                     throw gcnew ArgumentException("H5Dread failed!");
                 }
 
-                delete [] buf;
-
                 m_array = gcnew array<PSObject^>(dims[0]);
                 if (isCompound)
-                {
+                {   
+                    int mcount = H5Tget_nmembers(mem_type);
+                    array<String^>^ mname = gcnew array<String^>(mcount);
+                    array<int>^ msize = gcnew array<int>(mcount);
+                    array<int>^ moffset = gcnew array<int>(mcount);
+                    array<MethodInfo^>^ minfo = gcnew array<MethodInfo^>(mcount);
+
+                    Type^ magicType = System::BitConverter::typeid;
+
+                    for (int i = 0; i < mcount; ++i)
+                    {
+                        moffset[i] = safe_cast<int>(H5Tget_member_offset(mem_type, i));
+                        char* name = H5Tget_member_name(mem_type, safe_cast<unsigned>(i));
+                        mname[i] = gcnew String(name);
+                        hid_t mtype = H5Tget_member_type(mem_type, safe_cast<unsigned>(i));
+                        msize[i] = safe_cast<int>(H5Tget_size(mtype));
+                        
+                        H5T_class_t cls = H5Tget_class(mtype);
+                        switch (cls)
+                        {
+                        case H5T_INTEGER:
+                            if (ProviderUtils::H5NativeType2DotNet(mtype) == Int32::typeid) {
+                                minfo[i] = magicType->GetMethod("ToInt32");
+                            }
+                            else if (ProviderUtils::H5NativeType2DotNet(mtype) == Int64::typeid) {
+                                minfo[i] = magicType->GetMethod("ToInt64");
+                            }
+                            else if (ProviderUtils::H5NativeType2DotNet(mtype) == Int16::typeid) {
+                                minfo[i] = magicType->GetMethod("ToInt16");
+                            }
+                            if (ProviderUtils::H5NativeType2DotNet(mtype) == UInt32::typeid) {
+                                minfo[i] = magicType->GetMethod("ToUInt32");
+                            }
+                            else if (ProviderUtils::H5NativeType2DotNet(mtype) == UInt64::typeid) {
+                                minfo[i] = magicType->GetMethod("ToUInt64");
+                            }
+                            else if (ProviderUtils::H5NativeType2DotNet(mtype) == UInt16::typeid) {
+                                minfo[i] = magicType->GetMethod("ToUInt16");
+                            }
+                            else {
+                                minfo[i] = nullptr;
+                            }
+                            break;
+                        case H5T_FLOAT:
+                            if (ProviderUtils::H5NativeType2DotNet(mtype) == Double::typeid) {
+                                minfo[i] = magicType->GetMethod("ToDouble");
+                            }
+                            else if (ProviderUtils::H5NativeType2DotNet(mtype) == Single::typeid) {
+                                minfo[i] = magicType->GetMethod("ToSingle");
+                            }
+                            else {
+                                minfo[i] = nullptr;
+                            }
+                            break;
+                        default:
+                            minfo[i] = nullptr;
+                            break;
+                        }
+
+                        H5Tclose(mtype);
+                    }
+
+                    array<unsigned char>^ row = gcnew array<unsigned char>(size); 
+
                     for (long long i = 0; i < m_array->LongLength; ++i)
                     {
+                        Array::Copy(mbuf, size*i, row, 0, size);
+
                         m_array[i] = gcnew PSObject();
-                        for each (String^ key in ((Hashtable^)type["Members"])->Keys)
+
+                        for (int m = 0; m < mcount; ++m)
                         {
-                            m_array[i]->Properties->Add(gcnew PSNoteProperty(key,i));
+                            if (minfo[m] != nullptr) {
+                                m_array[i]->Properties->Add(
+                                    gcnew PSNoteProperty(mname[m],
+                                        minfo[m]->Invoke(nullptr,
+                                            gcnew array<Object^>{row, moffset[m]})));
+                            }
+                            else {
+                                m_array[i]->Properties->Add(
+                                    gcnew PSNoteProperty(mname[m],
+                                        BitConverter::ToString(row, moffset[m],
+                                            msize[m])));
+                            }
                         }
                     }
                 }
                 else
                 {
+                    array<unsigned char>^ row = gcnew array<unsigned char>(size);
+
                     for (long long i = 0; i < m_array->LongLength; ++i)
-                    {
-                        m_array[i] = gcnew PSObject(i);
+                    {    
+                        Array::Copy(mbuf, size*i, row, 0, size);
+                        m_array[i] = gcnew PSObject(BitConverter::ToString(row, 0, size));
                     }
                 }
             }
