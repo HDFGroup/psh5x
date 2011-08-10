70dd60a59c330f9360eb747cf68f0da3c5d3cc86
gheber <gheber@bc1736a7-42db-4d61-a5e1-31a1cdc50de3>
2011-08-10 17:51:47 +0000

    Added as simple example in Python/h5py and PowerShell.
    git-svn-id: https://svn.hdfgroup.org/PSH5X/trunk@36 bc1736a7-42db-4d61-a5e1-31a1cdc50de3

---
+++ doc/docbook/examples/dataset.ps1
@@ -0,0 +1,21 @@
+
+Add-PSSnapin HDF5
+
+New-PSDrive -Name 'sample1' -PSProvider HDF5 -Root sample1:\ `
+    -Path .\sample1.h5 -Force
+
+New-Item -Path sample1:\floats -ItemType Dataset `
+    -ElementType H5T_IEEE_F64LE -Dimensions @(256,512) `
+    -MaxDimensions @(-1,-1) -Chunked @(32,64) -Gzip 7
+
+$data = New-Object 'double[,]' 256,512
+
+for ($i = 0; $i -lt 256; $i++)
+{
+    $data[$i, $i] = $i;
+    $data[$i, (256+$i)] = $i;
+}
+
+Set-Content sample1:\floats $data
+
+Remove-PSDrive sample1
---
+++ doc/docbook/examples/dataset.py
@@ -0,0 +1,25 @@
+
+import h5py
+from h5py import *
+
+import numpy
+from numpy import zeros, float64
+
+h5file = h5f.create('sample1.h5', h5f.ACC_TRUNC)
+dspace = h5s.create_simple((256,512), (h5s.UNLIMITED, h5s.UNLIMITED))
+dtype = h5t.IEEE_F64LE.copy()
+
+plist = h5p.create(h5p.DATASET_CREATE)
+plist.set_chunk((32,64))
+plist.set_deflate(7)
+
+dset = h5d.create(h5file, 'floats', dtype, dspace, plist)
+
+data = zeros((256,512), dtype=float64)
+for i in range(256):
+    data[i,i] = i
+    data[i,256+i] = i
+
+dset.write(dspace, dspace, data)
+
+h5file.close()
