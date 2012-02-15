3b73784d84b774f11f6a6012fb5548acbc451ad3
gheber <gheber@bc1736a7-42db-4d61-a5e1-31a1cdc50de3>
2012-02-15 15:12:16 +0000

    Added aliases.
    git-svn-id: https://svn.hdfgroup.org/PSH5X/trunk@112 bc1736a7-42db-4d61-a5e1-31a1cdc50de3

---
+++ module/Aliases.ps1
@@ -0,0 +1,14 @@
+
+Set-Alias Get-H5Attribute Get-ItemProperty -Scope 1
+
+Set-Alias Get-H5Object Get-Item -Scope 1
+
+Set-Alias Remove-H5Attribute Remove-ItemProperty -Scope 1
+
+Set-Alias Rename-H5Attribute Rename-ItemProperty -Scope 1
+
+Set-Alias Remove-H5Object Remove-Item -Scope 1
+
+Set-Alias Rename-H5Object Rename-Item -Scope 1
+
+Set-Alias Unlink-H5Object Remove-Item -Scope 1
