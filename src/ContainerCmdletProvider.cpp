9939b37a33bdb41f9e3af54cd30e6297f6cbd106
gheber <gheber@bc1736a7-42db-4d61-a5e1-31a1cdc50de3>
2012-02-23 23:02:44 +0000

    First implementation of CopyItem.
    git-svn-id: https://svn.hdfgroup.org/PSH5X/trunk@118 bc1736a7-42db-4d61-a5e1-31a1cdc50de3

--- src/ContainerCmdletProvider.cpp
+++ src/ContainerCmdletProvider.cpp
@@ -31,9 +31,88 @@ namespace PSH5X
         WriteVerbose(String::Format(
             "HDF5Provider::CopyItem(Path = '{0}', CopyPath = '{1}', Recurse = {2})",
             path, copyPath, recurse));
-
-        throw gcnew PSH5XException("The HDF5Provider::CopyItem() method has not (yet) been implemented.");
         
+		hid_t plist = H5Pcreate(H5P_OBJECT_COPY), oid = -1;
+
+		char *path_str = NULL, *copy_path_str = NULL;
+		
+		try
+		{
+			DriveInfo^ drive = nullptr;
+            String^ h5path = nullptr;
+            if (!ProviderUtils::TryGetDriveEtH5Path(path, ProviderInfo, drive, h5path)) {
+                throw gcnew PSH5XException("Ill-formed HDF5 path name and/or unable to obtain drive name!");
+            }
+
+			DriveInfo^ copyDrive = nullptr;
+            String^ copyH5Path = nullptr;
+            if (!ProviderUtils::TryGetDriveEtH5Path(copyPath, ProviderInfo, copyDrive, copyH5Path)) {
+                throw gcnew PSH5XException("Ill-formed HDF5 path name and/or unable to obtain drive name!");
+            }
+
+			path_str = (char*)(Marshal::StringToHGlobalAnsi(h5path)).ToPointer();
+			copy_path_str = (char*)(Marshal::StringToHGlobalAnsi(copyH5Path)).ToPointer();
+
+			// TODO: check source and destination
+
+			if (!recurse) {
+				if(H5Pset_copy_object(plist, H5O_COPY_SHALLOW_HIERARCHY_FLAG) < 0) {
+					throw gcnew HDF5Exception("H5Pset_copy_object failed.");
+				}
+			}
+
+			bool ignoreAttributes = false;
+            RuntimeDefinedParameterDictionary^ dynamicParameters =
+                (RuntimeDefinedParameterDictionary^) DynamicParameters;
+            if (dynamicParameters != nullptr && dynamicParameters->ContainsKey("IgnoreAttributes")) {
+                ignoreAttributes = dynamicParameters["IgnoreAttributes"]->IsSet;
+            }
+			if (ignoreAttributes) {
+				if(H5Pset_copy_object(plist, H5O_COPY_WITHOUT_ATTR_FLAG) < 0) {
+					throw gcnew HDF5Exception("H5Pset_copy_object failed.");
+				}
+			}
+
+			if (H5Ocopy(drive->FileHandle, path_str, copyDrive->FileHandle, copy_path_str, plist, H5P_DEFAULT) >= 0)
+			{
+				if (H5Fflush(copyDrive->FileHandle, H5F_SCOPE_LOCAL) >= 0)
+				{
+					oid = H5Oopen(copyDrive->FileHandle, copy_path_str, H5P_DEFAULT);
+					if (oid >= 0) {
+						WriteItemObject(gcnew ObjectInfo(oid), copyPath,
+							ProviderUtils::IsH5Group(copyDrive->FileHandle, copyH5Path));
+					}
+					else {
+						throw gcnew HDF5Exception("H5Oopen failed!!!");
+					}
+				}
+				else {
+					throw gcnew HDF5Exception("H5Fflush failed!!!");
+				}
+			}
+			else {
+				throw gcnew HDF5Exception("H5Ocopy failed!!!");
+			}
+		}
+		finally
+		{
+			if (plist >= 0) {
+                H5Pclose(plist);
+            }
+
+			if (path_str != NULL) {
+                Marshal::FreeHGlobal(IntPtr(path_str));
+            }
+
+			if (copy_path_str != NULL) {
+                Marshal::FreeHGlobal(IntPtr(copy_path_str));
+            }
+
+			if (oid >= 0) {
+                H5Oclose(oid);
+            }
+		}
+
         return;
     }
 
@@ -43,7 +122,18 @@ namespace PSH5X
             "HDF5Provider::CopyItemDynamicParameters(Path = '{0}', CopyPath = '{1}', Recurse = {2})",
             path, copyPath, recurse));
 
-        return nullptr;
+        RuntimeDefinedParameterDictionary^ dynamicParameters =
+            gcnew RuntimeDefinedParameterDictionary();
+
+        Collection<Attribute^>^ atts1 = gcnew Collection<Attribute^>();
+        ParameterAttribute^ paramAttr1 = gcnew ParameterAttribute();
+        paramAttr1->Mandatory = false;
+        paramAttr1->ValueFromPipeline = false;
+        atts1->Add(paramAttr1);
+        dynamicParameters->Add("IgnoreAttributes",
+            gcnew RuntimeDefinedParameter("IgnoreAttributes", SwitchParameter::typeid, atts1));
+        
+        return dynamicParameters;
     }
 
     void Provider::GetChildItems(System::String^ path, bool recurse)
