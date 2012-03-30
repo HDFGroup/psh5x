66e5223299d7c97c7365c3c508c91fd14ff5f418
gheber <gheber@bc1736a7-42db-4d61-a5e1-31a1cdc50de3>
2012-03-30 15:01:38 +0000

    Support hyperslabs in [Get,Set]-H5DatasetValue.
    git-svn-id: https://svn.hdfgroup.org/PSH5X/trunk@204 bc1736a7-42db-4d61-a5e1-31a1cdc50de3

--- module/Get-H5DatasetValue.ps1
+++ module/Get-H5DatasetValue.ps1
@@ -6,8 +6,18 @@ Function Get-H5DatasetValue
       Gets the value of an HDF5 dataset
     .PARAMETER Path
       The path to an HDF5 dataset
+    .PARAMETER Start
+      The (multidimensional) offset where the hyperslab selection begins
+    .PARAMETER Stride
+      The (multidimensional) offset between consecutive blocks in the
+      hyperslab selection
+    .PARAMETER Count
+      The (mulitdimensional) block count of the hyperslab selection
+    .PARAMETER Block
+      The (mulitdimensional) block size of the hyperslab selection
     .LINK
       Get-Content
+      http://www.hdfgroup.org/HDF5/doc/UG/12_Dataspaces.html#DTransfer
  #>
     param
     (
@@ -16,14 +26,63 @@ Function Get-H5DatasetValue
                    HelpMessage='The path to an HDF5 dataset')]
         [ValidateNotNull()]
         [string]
-        $Path
+        $Path,
+        [Parameter(Mandatory=$false,
+                   ParameterSetName='Hyperslab',
+                   Position=2,
+                   HelpMessage='The start of the hyperslab selection')]
+        [ValidateNotNull()]
+        [uint64[]]
+        $Start,
+        [Parameter(Mandatory=$false,
+                   ParameterSetName='Hyperslab',
+                   Position=3,
+                   HelpMessage='The stride of the hyperslab selection')]
+        [ValidateNotNull()]
+        [uint64[]]
+        $Stride,
+        [Parameter(Mandatory=$false,
+                   ParameterSetName='Hyperslab',
+                   Position=4,
+                   HelpMessage='The block count of the hyperslab selection')]
+        [ValidateNotNull()]
+        [uint64[]]
+        $Count,
+        [Parameter(Mandatory=$false,
+                   ParameterSetName='Hyperslab',
+                   Position=5,
+                   HelpMessage='The block size of the hyperslab selection')]
+        [ValidateNotNull()]
+        [uint64[]]
+        $Block
     )
 
     if (!(Test-Path $Path -Resolvable))
     {
-        Write-Error "`nDataset '$Path' not found!!!"
+        Write-Error "`nDataset '$Path' not found!"
         return
     }
 
-    Get-Content $Path -ReadCount 0
+    if (!($Start -or $Stride -or $Count -or $Block))
+    {
+        Get-Content $Path -ReadCount 0
+    }
+    else
+    {
+        $cmd = 'Get-Content $Path -ReadCount 0'
+        if ($Start) {
+            $cmd += ' -Start $Start'
+        }
+        if ($Stride) {
+            $cmd += ' -Stride $Stride'
+        }
+        if ($Count) {
+            $cmd += ' -Count $Count'
+        }
+        if ($Block) {
+            $cmd += ' -Block $Block'
+        }
+
+        Invoke-Expression $cmd
+    }
 }
--- module/Set-H5DatasetValue.ps1
+++ module/Set-H5DatasetValue.ps1
@@ -8,8 +8,18 @@ Function Set-H5DatasetValue
       The path to an HDF5 dataset
     .PARAMETER Value
       The value of the HDF5 dataset
+    .PARAMETER Start
+      The (multidimensional) offset where the hyperslab selection begins
+    .PARAMETER Stride
+      The (multidimensional) offset between consecutive blocks in the
+      hyperslab selection
+    .PARAMETER Count
+      The (mulitdimensional) block count of the hyperslab selection
+    .PARAMETER Block
+      The (mulitdimensional) block size of the hyperslab selection
     .LINK
       Set-Content
+      http://www.hdfgroup.org/HDF5/doc/UG/12_Dataspaces.html#DTransfer
  #>
     param
     (
@@ -24,14 +34,63 @@ Function Set-H5DatasetValue
                    HelpMessage='The value of the HDF5 dataset')]
         [ValidateNotNull()]
         [object]
-        $Value
+        $Value,
+        [Parameter(Mandatory=$false,
+                   ParameterSetName='Hyperslab',
+                   Position=3,
+                   HelpMessage='The start of the hyperslab selection')]
+        [ValidateNotNull()]
+        [uint64[]]
+        $Start,
+        [Parameter(Mandatory=$false,
+                   ParameterSetName='Hyperslab',
+                   Position=4,
+                   HelpMessage='The stride of the hyperslab selection')]
+        [ValidateNotNull()]
+        [uint64[]]
+        $Stride,
+        [Parameter(Mandatory=$false,
+                   ParameterSetName='Hyperslab',
+                   Position=5,
+                   HelpMessage='The block count of the hyperslab selection')]
+        [ValidateNotNull()]
+        [uint64[]]
+        $Count,
+        [Parameter(Mandatory=$false,
+                   ParameterSetName='Hyperslab',
+                   Position=6,
+                   HelpMessage='The block size of the hyperslab selection')]
+        [ValidateNotNull()]
+        [uint64[]]
+        $Block
     )
 
     if (!(Test-Path $Path -Resolvable))
     {
-        Write-Error "`nDataset '$Path' not found!!!"
+        Write-Error "`nDataset '$Path' not found!"
         return
     }
 
-    Set-Content $Path $Value
+    if (!($Start -or $Stride -or $Count -or $Block))
+    {
+        Set-Content $Path $Value
+    }
+    else
+    {
+        $cmd = 'Set-Content $Path $Value'
+        if ($Start) {
+            $cmd += ' -Start $Start'
+        }
+        if ($Stride) {
+            $cmd += ' -Stride $Stride'
+        }
+        if ($Count) {
+            $cmd += ' -Count $Count'
+        }
+        if ($Block) {
+            $cmd += ' -Block $Block'
+        }
+
+        Invoke-Expression $cmd
+    }
 }
